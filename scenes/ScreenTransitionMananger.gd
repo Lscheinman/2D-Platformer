ea113a7f9413811649f1482cb34c30a7dfb0cd11
Lscheinman <40657768+Lscheinman@users.noreply.github.com>
2022-04-18 13:34:33 +0200

    Bug fixes

--- scenes/ScreenTransitionMananger.gd
+++ scenes/ScreenTransitionMananger.gd
@@ -4,6 +4,10 @@ var ScreenTransitionScene = preload("res://scenes/UI/Screentransition.tscn")
 
 
 func transition_to_scene(scenePath):
+	for button in get_tree().get_nodes_in_group("animated_button"):
+		button.disabled = true
+	
+	yield(get_tree().create_timer(.2), "timeout")
 	var ScreenTransition = ScreenTransitionScene.instance()
 	add_child(ScreenTransition)
 	yield(ScreenTransition, "screen_covered")
